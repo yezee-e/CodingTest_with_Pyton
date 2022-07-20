12dda62dccea68fc614f5759a675bcadf9d6cc74
yezee-e <98735544+yezee-e@users.noreply.github.com>
2022-07-21 01:27:05 +0900

    정렬

--- sort/sort.py
+++ sort/sort.py
@@ -26,11 +26,23 @@ print(array)
 #삽입정렬
 array=[7,5,9,0,3,1,6,2,4,8]
 
-for i in range(1,len(array)):
-    for j in range(i,0,-1): #인덱스 i부터 1ㅅ까지 1씩 감소하며 반복하는 문법
+for i in range(1,len(array)): #두번째원소부터시작
+    for j in range(i,0,-1): #인덱스 i부터 1까지 1씩 감소하며 반복하는 문법
         if array[j]<array[j-1]:
             array[j],array[j-1]=array[j-1],array[j]
         else:
             break #자기보다 작은 데이터를 만나면 그 위치에서 멈춤
 print(array)
 
+#삽입정렬2
+def ins_sort(a):
+    for i in range(1,len(a)):
+        key=a[i] #i번 위치에 있는 값을 key에 저장
+        j=i-1 #j를 i바로 왼쪽 위치로 저장
+        while j>=0 and a[j]>key: #리스트j번 위치에 있는 값과 key를 비교해 key가 삽입될 적절한 위치를 찾음
+            a[j+1]=a[j] #삽입할 공간이 생기도록 값을 오른쪽으로 한칸이동
+            j -=1
+        a[j+1]=key
+array=[2,4,5,1,3]
+ins_sort(array)
+print(array)
