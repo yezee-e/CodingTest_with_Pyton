9423c072f26af97da960413f9ce18d93ef7e5bd2
yezee-e <98735544+yezee-e@users.noreply.github.com>
2022-07-22 17:11:46 +0900

    정렬

--- sort/sort.py
+++ sort/sort.py
@@ -93,6 +93,23 @@ def quick_sort(array):
     return quick_sort(left_side)+[pivot]+quick_sort(right_side)
 
 print(quick_sort(array))
+
+#계수정렬
+
+#모든 원소의 값이 0보다 크거나 같다고 가정
+array=[7,5,9,0,3,1,6,2,9,1,4,8,0,5,2]
+#도든 범위를 포함하는 리스트 선언(모든 값은 0으로 초기화)
+count=[0]*(max(array)+1)
+
+for i in range(len(array)):
+    count[array[i]]+=1 #각 데이터에 해당하는 인덱스의 값 증가
+
+for i in range(len(count)): #리스트에 기록된 정렬정보 확인
+    for j in range(count[i]):
+        print(i,end='')
+
+
+
     
 
 
